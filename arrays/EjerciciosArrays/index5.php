eca888dfcba502df7030e0a7e318feea3df5191a
lorenzoea12 <92109963+lorenzoea12@users.noreply.github.com>
2022-10-28 11:00:31 +0200

    commit retoque de arrays

--- arrays/EjerciciosArrays/index5.php
+++ arrays/EjerciciosArrays/index5.php
@@ -11,15 +11,32 @@
 
 <body>
     <?php
+    /*
     $estudiantes = [
         ["Lorenzo", rand(0, 10)],
         ["Pedro", rand(0, 10)],
         ["Manuel", rand(0, 10)],
         ["Moises", rand(0, 10)],
         ["Antonio", rand(0, 10)],
-
-
     ];
+    */
+
+   
+
+    $estudiantes = [
+        ["Luis"],
+        ["Alfredo"],
+        ["Elena"]
+    ];
+
+    for ($i = 0; $i < count($estudiantes); $i++) {
+        $estudiantes[$i][1] = rand(0,10);
+        $estudiantes[$i][2] = rand(0,10);
+        $estudiantes[$i][3] = rand(0,10);
+    }
+
+
+
     function calificacion($nota){
         $calificacion_final = match($nota){
         0,1,2,3,4 =>"suspenso",
@@ -33,11 +50,41 @@
     return $calificacion_final;
     }
 
+
+    function calificacion1($nota){
+        switch($nota){
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+                $calificacion1= "Suspenso";
+                break;
+            case 5:
+                $calificacion1= "Suficiente";
+                break;
+            case 6:
+                $calificacion1= "Bien";
+            case 7:
+            case 8:
+                  $calificacion1="Notable";
+            case 9:
+            case 10:
+                $calificacion1= "Sobresaliente";
+                break;
+            default: 
+            $calificacion1=  "La nota no es valida";
+                
+        }
+        return $calificacion1;
+    }
+
     ?>
     <table>
         <tr>
             <th>Nombre</th>
-            <th>Nota</th>
+            <th>Nota 1</th>
+            <th>Nota 2</th>
+            <th>Nota 3</th>
             <th>CalificacionFinal</th>
         </tr>
         <?php
@@ -46,12 +93,15 @@
         array_multisort($nombre, SORT_ASC, $estudiantes);
        
         foreach ($estudiantes as $estudiante) {
-            list($nombre, $nota) = $estudiante;
+            list($nombre, $nota1,$nota2,$nota3) = $estudiante;
+            $media=(int)round($nota1+$nota2+$nota3)/10;
         ?>
             <tr>
                 <td><?php echo $nombre ?></td>
-                <td><?php echo $nota ?></td>
-                <td><?php echo calificacion($nota)?></td>
+                <td><?php echo $nota1 ?></td>
+                <td><?php echo $nota2 ?></td>
+                <td><?php echo $nota3 ?></td>
+                <td><?php echo calificacion($media)?></td>
 
                 
             </tr>
